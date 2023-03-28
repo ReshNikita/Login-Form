c8277f1c8b276df8430fb509ecaf9f899b29e3b8
ReshNikita <rieshietnikov.nikita.1998@mail.ru>
2023-03-29 02:40:01 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import Login from "./Components/Login";
-import "./Styles/App.css";
-import "antd/dist/reset.css";
 import { Routes, Route } from "react-router-dom";
 import ErrorComponent from "./Components/ErrorComponent";
 import RegistrationComponent from "./Components/RegistrationComponent";
 import Layout from "./Components/Layout";
 
+import "./App.css";
+import "antd/dist/reset.css";
+
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Login />} />
-          <Route path="register" element={<RegistrationComponent />} />
-          <Route path="*" element={<ErrorComponent />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Login />} />
+        <Route path="register" element={<RegistrationComponent />} />
+        <Route path="*" element={<ErrorComponent />} />
+      </Route>
+    </Routes>
   );
 };
 
